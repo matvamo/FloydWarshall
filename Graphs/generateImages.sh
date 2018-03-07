3f2613456b57f92beffd7d498174a0eb7e5324e4
Mathijs van Mourick <m.vanmourick@icloud.com>
2018-03-07 14:46:00 +0100

    Add GraphViz dotfiles and image renders of graphs used in program unit test

---
+++ Graphs/generateImages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+dot  -Tpng graph1.dot > graph1.png
+dot -Tpng graph2.dot > graph2.png
+circo -Tpng graph3.dot > graph3.png
+circo -Tpng graph4.dot > graph4.png
+circo -Tpng graph5.dot > graph5.png
